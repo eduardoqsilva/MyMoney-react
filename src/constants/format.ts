3049b5132e1e2fd81eca6b7894a13f009209a0e9
eduardoqsilva <eduadsilva@outlook.com>
2023-04-18 18:53:21 -0300

    add a date get function

--- src/constants/format.ts
+++ src/constants/format.ts
@@ -1,4 +1,15 @@
 export const Format = Intl.NumberFormat('pt-BR', {
   style: 'currency',
-  currency: 'BRL'
+  currency: 'BRL',
+  minimumFractionDigits: 2,  
+  maximumFractionDigits: 2,  
 })
+
+export const GetDateF = () => {
+  const date = new Date();
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+  const formattedDate = `${day}/${month}/${year}`;
+  return formattedDate
+}
