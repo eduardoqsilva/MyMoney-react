2df22bf8640907f35298dfd09b0bec2437d1d708
eduardoqsilva <eduadsilva@outlook.com>
2023-04-18 18:55:00 -0300

    update status component

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -1,26 +1,44 @@
 import { Card } from "../Card";
 import { Grid, StatusContainer } from "./Status.style";
+import { ListType } from "../../pages/Transactions";
 
-export function Status() {
+
+interface TypeStatus {
+  list: ListType[]
+}
+
+
+export function Status({ list }:TypeStatus) {
+  let values = [0,0,0]
+
+  list.map((i) => {
+    if(i.value > 0) {
+      values[0] = values[0] + i.value
+    }else {
+      values[1] = values[1] + i.value
+    }
+    values[2] = values[2] + i.value
+  })
+
   return (
     <StatusContainer>   
       <Grid>
         <Card 
-          value={17000}
+          value={values[0]}
           legend={'Entradas'}
-          positive={true}
+          positive={values[0] > 1}
           variation={'01'}
         />
         <Card 
-          value={17000}
+          value={values[1]}
           legend={'Saídas'}
-          positive={false}
+          positive={values[1] > 1}
           variation={'01'}
         />
         <Card 
-          value={17000}
+          value={values[2]}
           legend={'Total'}
-          positive={true}
+          positive={values[2] > 1}
           variation={'02'}
         />
       </Grid>
