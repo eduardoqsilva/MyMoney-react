e526b8a2438f126f07f599ee5ad88510cd67f493
eduardoqsilva <eduadsilva@outlook.com>
2023-04-18 18:55:45 -0300

    update component search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,23 @@
 import { FileSearch, MagnifyingGlass } from "@phosphor-icons/react";
+import { ChangeEvent } from "react";
 import { SearchButton, SearchInput, SearchWrapperStyled } from "./search.styled";
 
-export function Search() {
+interface SearchType {
+  setSearch: (str:string) => void
+}
+
+let value = ''
+
+export function Search({setSearch}:SearchType) {
+
+  const current = (e:ChangeEvent<HTMLInputElement>) => {
+    value = e.currentTarget.value
+  }
+
   return (
     <SearchWrapperStyled>
-      <SearchInput  placeholder="Busque uma transação" />
-      <SearchButton>
+      <SearchInput  placeholder="Busque uma transação"  onChange={(e) => current(e)}/>
+      <SearchButton onClick={() => setSearch(value.trim())}>
         <MagnifyingGlass weight="bold" size={20}/>
         Buscar
       </SearchButton>
